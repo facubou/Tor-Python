4b6ba387de3482baeb321887aed9c9037272c879
Facundo Bouza <facundo.bouza@gmail.com>
2018-10-08 07:56:54 -0300

    Search ip

--- repetidores_Tor.py
+++ repetidores_Tor.py
@@ -1,7 +1,8 @@
 #script para traer ip addresses de repetidores tor utilizando la libreria stem
-
+from colorama import init, Fore, Back, Style
 from stem.descriptor.remote import DescriptorDownloader
 import re
+init()
 cont = 0
 infoCompleta = ""
 downloader = DescriptorDownloader()
@@ -12,14 +13,30 @@ for descriptor in downloader.get_consensus().run():
     parse = "".join(ipFind)
     infoCompleta = infoCompleta + "\n" + str(parse)
     cont = cont + 1
+#print(ipFind, parse, cont)
     
-
+print("---------Todos los Repetidores TOR------------")
 print (infoCompleta)
-print ("Existen: " + str(cont) + " nodos tor")
+print ("\nExisten: " + str(cont) + " nodos tor actualmente\n")
 try:
   f = open("export.txt", "a")
   f.write(infoCompleta)
   f.close
-  print ("se guardaron los datos en el archivo txt")
+  print ("Se guardaron los datos en el archivo txt\n")
 except:
-  print ("no se pudo generar archivo txt")
+  print ("no se pudo generar archivo txt\n")
+  
+search = input("Buscar IP? (y o n): ")
+if search == "y":
+  ip = input("Ip a buscar(EJ:10.0.0.0): ")
+  c = infoCompleta.split()
+  #print(c)
+  try:
+    b=c.index(ip)
+    print(Fore.RED + ip, "es un Tor Exit Node\n")
+  except ValueError:
+    print(Fore.GREEN + ip, "No es Tor Exit Node\n")
+else:
+  print(Fore.WHITE + "Cerrando aplicación...\n")
+
+print(Fore.WHITE + "Proceso finalizado")
